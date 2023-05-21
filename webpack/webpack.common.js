25f11d05c8387e2b20290cc48df44b0e04c3bdd0
Maxim <maximtsoy2001@gmail.com>
2023-05-21 20:29:40 +0600

    webpack rework

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
     extensions: [".js", ".ts", ".tsx", ".jsx"],
   },
   output: {
-    path: path.resolve(__dirname, "../build"),
+    path: path.resolve(__dirname, "../dist"),
     filename: "bundle.js",
   },
   plugins: [
