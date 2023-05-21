25ad19a07aeffdaae3ae16b049b21ed94e1d264c
Maxim <maximtsoy2001@gmail.com>
2023-05-21 20:26:38 +0600

    webpack rework

--- src/components/QuickOptions/QuickOptions.tsx
+++ src/components/QuickOptions/QuickOptions.tsx
@@ -3,8 +3,12 @@ import Box from "../../utils/Box";
 import { useAppSelector } from "../../state";
 import InlineButton from "../../utils/InlineButton";
 import Category from "../../utils/Category";
-import CodeOffRoundedIcon from '@mui/icons-material/CodeOffRounded';
-import AutoGraphRoundedIcon from '@mui/icons-material/AutoGraphRounded';
+import CodeOffRoundedIcon from "@mui/icons-material/CodeOffRounded";
+import AutoGraphRoundedIcon from "@mui/icons-material/AutoGraphRounded";
+import PaletteIcon from "@mui/icons-material/Palette";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import CameraEnhanceIcon from "@mui/icons-material/CameraEnhance";
 
 const QuickOptions = () => {
   const { likes, comments, playlists } = useAppSelector((state) => state.user);
@@ -31,65 +35,28 @@ const QuickOptions = () => {
         <div className="box">
           <h3 className="title">top categories</h3>
           <div className="flex">
-           
-            <Category name="development" icon={<CodeOffRoundedIcon/>} />
-            <Category name="business" icon={<AutoGraphRoundedIcon/>}  />
-              
-            
-            <a href="#">
-              <i className="fas fa-pen"></i>
-              <span>design</span>
-            </a>
-            <a href="#">
-              <i className="fas fa-chart-line"></i>
-              <span>marketing</span>
-            </a>
-            <a href="#">
-              <i className="fas fa-music"></i>
-              <span>music</span>
-            </a>
-            <a href="#">
-              <i className="fas fa-camera"></i>
-              <span>photography</span>
-            </a>
-            <a href="#">
-              <i className="fas fa-cog"></i>
-              <span>software</span>
-            </a>
-            <a href="#">
-              <i className="fas fa-vial"></i>
-              <span>science</span>
-            </a>
+            <Category name="development" icon={<CodeOffRoundedIcon />} />
+            <Category name="business" icon={<AutoGraphRoundedIcon />} />
+            <Category name="design" icon={<PaletteIcon />} />
+            <Category name="marketing" icon={<StorefrontIcon />} />
+            <Category name="music" icon={<MusicNoteIcon />} />
+            <Category name="photography" icon={<CameraEnhanceIcon />} />
           </div>
         </div>
 
         <div className="box">
           <h3 className="title">popular topics</h3>
+
           <div className="flex">
-            <a href="#">
-              <i className="fab fa-html5"></i>
-              <span>HTML</span>
-            </a>
-            <a href="#">
-              <i className="fab fa-css3"></i>
-              <span>CSS</span>
-            </a>
-            <a href="#">
-              <i className="fab fa-js"></i>
-              <span>javascript</span>
-            </a>
-            <a href="#">
-              <i className="fab fa-react"></i>
-              <span>react</span>
-            </a>
-            <a href="#">
-              <i className="fab fa-php"></i>
-              <span>PHP</span>
-            </a>
-            <a href="#">
-              <i className="fab fa-bootstrap"></i>
-              <span>bootstrap</span>
-            </a>
+            <Category name="HTML" icon={<i className="fab fa-html5"></i>} />
+            <Category name="CSS" icon={<i className="fab fa-css3"></i>} />
+            <Category name="javascript" icon={<i className="fab fa-js"></i>} />
+            <Category name="react" icon={<i className="fab fa-react"></i>} />
+            <Category name="PHP" icon={<i className="fab fa-php"></i>} />
+            <Category
+              name="python"
+              icon={<i className="fa-brands fa-python"></i>}
+            />
           </div>
         </div>
 
---
+++ webpack/webpack.common.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const Dotenv = require("dotenv-webpack");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: path.resolve(__dirname, "../src/index.tsx"),
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "../build"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../public/index.html"),
+    }),
+    // new Dotenv({
+    //   systemvars: true,
+    // }),
+    // new webpack.LoaderOptionsPlugin({
+    //   options: {
+    //     sassResources: [path.resolve(__dirname, "../src/styles/color.scss")],
+    //   },
+    // }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+          // {
+          //   loader: "sass-resources-loader",
+          //   // options: {
+          //   //   resources: [
+          //   //     path.resolve(__dirname, "../src/styles/colors.scss"),
+          //   //     path.resolve(__dirname, "../src/styles/sizes.scss"),
+          //   //   ],
+          //   // },
+          // },
+        ],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
